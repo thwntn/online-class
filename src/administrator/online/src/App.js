4be23fe00748761f92972db16407886261720a88
Thien <nttansv@gmail.com>
2022-02-10 21:08:14 +0700

    add page admin

---
+++ src/administrator/online/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './navigation/header.js'
+import HomePage from './container/homepage.js'
+
+function App() {
+  return (
+      <div>
+        <Header></Header>
+        <HomePage></HomePage>
+      </div>
+  )
+}
+
+export default App;
---
+++ src/administrator/online/src/container/homepage.js
@@ -0,0 +1,7 @@
+function HomePage () {
+    const height = window.innerHeight
+    return (
+        <div style = {{width: '100%', height:  height, backgroundImage: 'url(./image/homepage.jpg', backgroundSize: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>Home Page</div>
+    )
+}
+export default HomePage
