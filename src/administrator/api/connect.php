5323dc76021f84b1d1c13654bbf835dcca2f9f16
Thien Tan <nttansv@gmail.com>
2022-03-26 21:30:04 +0700

    u f

--- src/administrator/api/connect.php
+++ src/administrator/api/connect.php
@@ -1,7 +1,3 @@
 <?php
-<<<<<<< HEAD
     $conn = new MySQLi('192.168.1.24', 'connect', 'thwntn', 'online-class') or die ('Error'. $conn->err);
-=======
-    $conn = new MySQLi ('192.168.1.24', 'connect', 'thwntn', 'online-class') or die($conn->err);
->>>>>>> 7708f7e8c6a500a3f71783446e887e969d9f2cac
 ?>
